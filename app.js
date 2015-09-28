fd71554168435798fdb038ff58105bfec3254709
berelindis <romain.bellande@orange.fr>
2015-09-28 12:53:47 +0200

    io.on - connection

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ var port = 8080;
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
-
+io.on('connection',function(socket){
+  console.log('a user connected');
+})
 http.listen(port);
